afde0b0241235f6e2d81df8009e34c8ab0101069
Camila Miorelli <camimiorelli@gmail.com>
2020-08-21 16:40:11 +0200

    Added a feature to upload photos by the user from cloudinary

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -20,7 +20,7 @@ class CocktailsController < ApplicationController
   private
 
   def strong_params
-    params.require("cocktail").permit(:name)
+    params.require("cocktail").permit(:name, :photo)
   end
 
 end
